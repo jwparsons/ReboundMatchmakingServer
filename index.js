a9033821bb76a87ae647846020553b7f22521b5d
James Parsons <jamespar@bu.edu>
2017-11-14 04:26:50 -0500

    server communication established

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var socket = require('socket.io');
+var execFile = require('child_process').execFile;
+var events = require('events');
+
+// handle server creation
+var eventEmitter = new events.EventEmitter();
+
+// keep track of player queue
+var clients = []
 
 // app setup
 var app = express();
@@ -10,6 +18,40 @@ var server = app.listen(3000, function() {
 // socket setup
 var io = socket(server);
 
+// create server when told to do so
+var createServer = function() {
+  // create new server
+  execFile('reboundRun.bat', function callback(err, data){
+    console.log(err);
+    console.log(data.toString());
+  });
+}
+eventEmitter.on('createServer', createServer);
+
+// handle server requests
 io.on('connection', function(socket) {
   console.log('made socket connection', socket.id);
+
+  socket.on('join', function(data) {
+    console.log(socket.id + ' wants to join a game');
+    clients.push(socket.id);
+    if (clients.length >= 4) {
+      eventEmitter.emit('createServer');
+    }
+  });
+
+  socket.on('cancel', function(data) {
+    clients.pop(socket.id);
+    console.log(socket.id + ' changed their mind');
+  });
+
+  socket.on('server creation', function(data) {
+    console.log('server has been created');
+    if (clients.length >= 4) {
+      // tell clients to join
+    }
+    else {
+      // emit to server to terminate
+    }
+  });
 });
---
+++ reboundRun.bat
@@ -0,0 +1,5 @@
+@echo off
+cd "F:\GitHub\ReboundMatchmakingServer\Rebound\Binaries\Win64"
+start Rebound.exe -log
+
+exit
